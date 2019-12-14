dd1f8f04eb860b1d911891869eb89d2457caa20d
Chetabahana <chetabahana@gmail.com>
2019-12-15 01:41:31 +0700

    npx webpack

--- test/fixtures/three.graphql/scripts/postinstall.sh
+++ test/fixtures/three.graphql/scripts/postinstall.sh
@@ -2,3 +2,4 @@
 
 # Copy graphiql.css to current directory
 cp -f node_modules/graphiql/graphiql.css .
+npx webpack
--- test/fixtures/three.graphql/scripts/preinstall.sh
+++ test/fixtures/three.graphql/scripts/preinstall.sh
@@ -5,14 +5,14 @@ sudo npm install npm@latest -g
 # Rename componentWillReceiveProps to UNSAFE_componentWillReceiveProps
 # Referrence: https://fb.me/react-derived-state
 echo -e "\n$hr\nNPX REACT\n$hr"
-rm -rf graphiql.css package-lock.json yarn.lock
 npx react-codemod rename-unsafe-lifecycles
 
 # Delete all regenerated files
+rm -rf graphiql.css package-lock.json yarn.lock
+
 echo -e "\n$hr\nINSTALL\n$hr"
-npm i -D babel-loader @babel/preset-env @babel/preset-react
-npm i -D webpack webpack-cli webpack-dev-server
-npm i -D html-loader html-webpack-plugin
-touch webpack.config.js
-touch .babelrc
-npx webpack
+#npm i -D babel-loader @babel/preset-env @babel/preset-react
+#npm i -D webpack webpack-cli webpack-dev-server
+#npm i -D html-loader html-webpack-plugin
+#touch webpack.config.js
+#touch .babelrc
